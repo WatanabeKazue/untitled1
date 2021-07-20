b5a140e184b9fb4ecead7f676401743ae9d11caf
WatanabeKazue <kazueWatanabe.202105@gmail.com>
2021-07-20 14:42:12 +0900

    エラーでています。

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 
@@ -10,6 +10,23 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScrState extends State<HomeScreen> {
+
+ // String _text01 = "おめでとうございます";
+ // String _text02 = "合格です";
+ //  String _text03 = "よくできました";
+ // String _text04 = "残念でした";
+  // String _text05 = "不合格です";
+ // String _text06 = "頑張りましょう";
+
+  List<String> _texts = [
+    "おめでとうございます",
+    "合格です",
+    "よくできました",
+    "残念でした",
+    "不合格です",
+    "頑張りましょう"
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,13 +34,56 @@ class _HomeScrState extends State<HomeScreen> {
         title: Text("ツッコミマシーン"),
         centerTitle: true,
       ),
-      body: Column(
-        children: <Widget>[
-       _soundButton(),
-       _soundBotton(),
+      body: Center(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Expanded(flex: 1, child: _soundButton()), //TODO ボタン
+                  Expanded(flex: 1,child: _soundButton()), //TODO ボタン
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Expanded(flex: 1,child: _soundButton()), //TODO ボタン
+                  Expanded(flex: 1,child: _soundButton()), //TODO ボタン
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Expanded(flex: 1,child: _soundButton()), //TODO ボタン
+                  Expanded(flex: 1,child: _soundButton()), //TODO ボタン
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
-     ],
+  Widget _soundButton() {
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      child: RaisedButton(
+
       ),
     );
   }
 }
+
+
+
+
+
+    
